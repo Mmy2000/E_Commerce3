0c6fda656545cbb82ecaf9c27ab1f974371ffc1d
Mmy2000 <yahianada005@gmail.com>
2023-12-14 23:35:39 +0200

    updating

--- category/models.py
+++ category/models.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 class Category(models.Model):
     category_name = models.CharField(unique=True, max_length=50)
     slug = models.SlugField(unique=True, max_length=100)
+    image = models.ImageField( upload_to='cat_img' , blank=True)
+    clas = models.CharField(max_length=50 ,blank=True ,null=True)
+    description = models.TextField(max_length=10000,blank=True ,null=True)
 
     class Meta:
         verbose_name = 'category'
@@ -21,6 +24,7 @@ class SubCategory(models.Model):
     subcategory_name = models.CharField(unique=True, max_length=50)
     slug = models.SlugField(unique=True, max_length=100)
     image = models.ImageField( upload_to='subcat_img' , blank=True)
+    description = models.TextField(max_length=10000,blank=True ,null=True)
 
     class Meta:
         verbose_name = 'subcategory'
