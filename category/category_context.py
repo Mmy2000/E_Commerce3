377bbacb58d76b0c74376e8df046e1c9116df9fd
Mmy2000 <yahianada005@gmail.com>
2023-12-14 22:52:18 +0200

    updating

--- category/category_context.py
+++ category/category_context.py
@@ -1,6 +1,7 @@
 from .models import Category
+from django.db.models import Count
 
 
 def menu_links(request):
-    links = Category.objects.all()[:10]
+    links = Category.objects.all().annotate(products_count=Count('product'))[:10]
     return dict(links=links)
