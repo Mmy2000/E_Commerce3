0afe52b775ccb19d1c51f8439b87a8ff983e8efc
Mmy2000 <yahianada005@gmail.com>
2023-12-16 22:14:41 +0200

    create cart functionallity part 7

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect , get_object_or_404
 from store.models import Product
 from .models import Cart , CartItem
-
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -13,6 +13,12 @@ def _cart_id(request):
     return cart
 
 def add_cart(request , product_id):
+    color= request.GET['color']
+    size = request.GET['size']
+    quantity = request.GET['quantity']
+    return HttpResponse(color + ' ' + size + ' ' + quantity)
+    exit()
+
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request))
